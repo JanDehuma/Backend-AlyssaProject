c6caceed6159a18e6b972350b2d631d2fce163ee
Jan Dehuma <dehuma-jan@hotmail.com>
2023-07-31 23:07:28 -0600

    Change the soft delete Propietarios

--- src/modules/propietarios/repositories/propietario.repository.ts
+++ src/modules/propietarios/repositories/propietario.repository.ts
@@ -1,53 +1,53 @@
 import { Prisma, PrismaClient } from "@prisma/client";
 import { Injectable } from "@tsed/di";
-import { PropietarioInputBorrar } from "../inputs/propietarioBorrado.input";
-import { PropietariosInput } from '../inputs/propietario.input';
-import { DeletePropietarioInput } from "../inputs/propietarios.delete.input";
+import { PropietariosInput } from "../inputs/propietario.input";
 
 @Injectable()
-export class PropietarioRepository{
-    constructor(private readonly prisma: PrismaClient){}
+export class PropietarioRepository {
+  constructor(private readonly prisma: PrismaClient) {}
 
-    getAllPropietarios(){
-        return this.prisma.propietarios.findMany();
-    }
+  getAllPropietarios() {
+    return this.prisma.propietarios.findMany();
+  }
 
-    getPropietario(id: number){
-        return this.prisma.propietarios.findUnique({
-            where: {
-              idPropietario: Number(id)
-            }
-          });
-    }
+  getPropietario(id: number) {
+    return this.prisma.propietarios.findUnique({
+      where: {
+        idPropietario: Number(id)
+      }
+    });
+  }
 
-    async createPropietario(input: PropietariosInput, idUsuario: number){
-        return await this.prisma.propietarios.create({
-            data: {
-                ... input,
-                usuario: {
-                    connect: {
-                        idUsuario: idUsuario
-                    }
-                }
-            }
-        });
-    }
+  async createPropietario(input: PropietariosInput, idUsuario: number) {
+    return await this.prisma.propietarios.create({
+      data: {
+        ...input,
+        usuario: {
+          connect: {
+            idUsuario: idUsuario
+          }
+        }
+      }
+    });
+  }
 
-    async deletePropietario(id: number, update: DeletePropietarioInput) {
-        return await  this.prisma.propietarios.update({
-            where: {
-                idPropietario: Number(id)
-            },
-            data: update
-        });
-    }
+  async deletePropietario(id: number) {
+    return await this.prisma.propietarios.update({
+      where: {
+        idPropietario: Number(id)
+      },
+      data: {
+        borrado: 1
+      }
+    });
+  }
 
-    async updatePropietario(id: number, input: Prisma.PropietariosUpdateInput){
-        return await this.prisma.propietarios.update({
-            where: {
-                idPropietario: Number(id)
-            },
-            data: input
-        });
-    }
+  async updatePropietario(id: number, input: Prisma.PropietariosUpdateInput) {
+    return await this.prisma.propietarios.update({
+      where: {
+        idPropietario: Number(id)
+      },
+      data: input
+    });
+  }
 }
